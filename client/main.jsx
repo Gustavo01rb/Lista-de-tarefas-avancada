ef1921fc41ce44cce660683df6e4df87781de174
Gustavo <gustavo05rb@gmail.com>
2023-08-07 17:01:30 -0300

    Adição do styled components

--- client/main.jsx
+++ client/main.jsx
@@ -1,9 +1,15 @@
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
-import { createRoot } from 'react-dom/client'; 
-import Routes from '../imports/routes/routes'
+import { createRoot } from 'react-dom'; // Importação corrigida
+import Routes from '../imports/routes/routes';
+import GlobalStyle from '../imports/ui/global/style';
 
 Meteor.startup(() => {
   const root = createRoot(document.getElementById('react-target'));
-  root.render(<Routes />);
+  root.render(
+    <>
+      <GlobalStyle />
+      <Routes />
+    </>
+  );
 });
